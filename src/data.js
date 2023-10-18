4fa5455c5f663dc1bb780164d1641cbbde61e345
Luke Martin <lmartin9@uiowa.edu>
2023-10-17 23:51:25 -0500

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const projects = [
+  {
+    title: "ToDo List",
+    subtitle: "JavaScript | HTML | CSS",
+    description:
+      "An app that allows a user to add, delete, and mark off tasks for their daily routines.",
+    image: "./todo-list.gif",
+    link: "https://64df242dae276d1e7d5c0aa5--subtle-kheer-5836aa.netlify.app/",
+  },
+  {
+    title: "Tik-Tak-Toe",
+    subtitle: "React and JavaScript",
+    description:
+      "This is a tik-tak-toe app that has a time travel feature, allowing a player to jump back to previous moves in a game.",
+    image: "tik-tak-toe.gif",
+    link: "https://incredible-unicorn-f54aaa.netlify.app",
+  },
+  {
+    title: "Recipe App",
+    subtitle: "React and JavaScript",
+    description:
+      "A recipe app using an API from the platform EDAMAM, where a user can search a keyword ingredient and all recipes containing that ingredient will be returned",
+    image: "recipe-app.gif",
+    link: "https://recipes.d48y44929mzyq.amplifyapp.com",
+  },
+  {
+    title: "Hospital Management System",
+    subtitle: "Flask | Python | MySQL",
+    description:
+      "A fully functional hospital management system web application that contains CRUD operations for functions such as scheduling appointments, assigning beds, and making payments.",
+    image: "HMS.gif",
+    link: "https://github.com/lukemartin3/Hospital-Management-System",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Java",
+  "Python",
+  "C++",
+  "JavaScript",
+  "SQL",
+  "NoSQL",
+  "HTML/CSS",
+  "Haskell",
+  "Git",
+  "Github",
+  "GitLab",
+  "React",
+  "Node.js",
+  "Flask",
+  "AWS",
+  "REST"
+];
